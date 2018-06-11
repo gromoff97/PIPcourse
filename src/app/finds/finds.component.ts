de6cf31ea8c5a79d236f3339ff4efca8fdf1f31d
gromoff97 <gromoff97@mail.ru>
2018-06-12 00:30:46 +0300

    finds are showed

--- src/app/finds/finds.component.ts
+++ src/app/finds/finds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-finds',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindsComponent implements OnInit {
 
-  constructor() { }
+  _http : HttpClient;
+
+  constructor( private gotHttp:HttpClient ) {
+  	this._http = gotHttp;
+  }
+
+  getNewFindContent( id : number, content:string ) : string
+  {
+  	return "<div class=\"window\"> <div class=\"name\">Находка №" + id + 
+  	"</div><div class=\"content\">"+ content + 
+  	"</div></div>"
+  }
 
   ngOnInit() {
+  	let data = this._http.get('http://localhost:8080/course/api/lostfound/all',{ observe: 'response' })
+  	.subscribe(data=>
+  	{
+  		let finds = document.getElementById("finds");
+  		let newsHtml : string = "";
+  		let finds_count : number = Object.keys(data.body).length;
+
+  		for ( let finds_counter : number = 0; finds_counter < finds_count; finds_counter++ ) 
+  			newsHtml = this.getNewFindContent(
+  				finds_counter + 1,
+  				data.body[finds_counter].message
+  				) + newsHtml;
+
+  		finds.innerHTML = newsHtml;
+  				
+  	});
   }
 
 }
