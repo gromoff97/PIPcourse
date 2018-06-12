9c5260bb786fa5cb7f04095c9d7bdb1c2f42fe4a
SpaceStar <spacestar.ru@gmail.com>
2018-06-12 16:02:42 +0300

    code for mainpage was beutified

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -8,78 +8,76 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainpageComponent implements OnInit {
 
-  _http : HttpClient;
-  stationFrom : string = null;
-  stationTo : string = null;
+  _http: HttpClient;
+  stationFrom: string = null;
+  stationTo: string = null;
   station_id_dict = [];
 
-  constructor( private gotHttp:HttpClient ) {
-  	this._http = gotHttp;
+  constructor( private gotHttp: HttpClient ) {
+    this._http = gotHttp;
   }
 
-  fillLastNewsPost()
-  {
-    let data = this._http.get('http://localhost:8080/course/api/news/all',{ observe: 'response' })
-    .subscribe(data=>
-    {
-      document.getElementById("lastnewspost").innerHTML = data.body[Object.keys(data.body).length - 1].content;    
+  fillLastNewsPost() {
+    this._http.get('http://localhost:8080/course/api/news/all', { observe: 'response' })
+    .subscribe(data => {
+      document.getElementById('lastnewspost').innerHTML = data.body[Object.keys(data.body).length - 1].content;
     });
   }
 
-  fillSelectors()
-  {
-     let data = this._http.get('http://localhost:8080/course/api/stations/all',{ observe: 'response' })
-    .subscribe(data=>
-    {
-      let station_selectors = document.getElementsByClassName("select-flex");
+  fillSelectors() {
+    this._http.get('http://localhost:8080/course/api/stations/all', { observe: 'response' })
+    .subscribe(data => {
+      const station_selectors = document.getElementsByClassName('select-flex');
 
-      let station_selectors_cnt = station_selectors.length;
-      let stations_cnt = Object.keys(data.body).length;
-      for ( let station_counter : number = 0; station_counter < stations_cnt; station_counter++ )
-      {
+      const station_selectors_cnt = station_selectors.length;
+      const stations_cnt = Object.keys(data.body).length;
+      for ( let station_counter = 0; station_counter < stations_cnt; station_counter++ ) {
         this.station_id_dict.push({
           station: data.body[station_counter].name,
           id: data.body[station_counter].id
         });
       }
 
-      for ( let station_selectors_counter = 0; station_selectors_counter < station_selectors_cnt; station_selectors_counter++ )
-        for ( let station_counter : number = 0; station_counter < stations_cnt; station_counter++ )
-        {
-          let option = document.createElement("option");
+      for ( let station_selectors_counter = 0; station_selectors_counter < station_selectors_cnt; station_selectors_counter++ ) {
+        for (let station_counter = 0; station_counter < stations_cnt; station_counter++) {
+          const option = document.createElement('option');
           option.innerHTML = data.body[station_counter].name;
           station_selectors[station_selectors_counter].appendChild(option);
         }
+      }
 
     });
   }
 
-  fillPathResult()
-  {
-    let res = document.getElementById("pathres");
-    if ( this.stationFrom === this.stationTo )
-    {
-      res.innerHTML = "Пожалуйста, выберите разные станции"
+  fillPathResult() {
+    const res = document.getElementById('pathres');
+    if ( this.stationFrom === this.stationTo ) {
+      res.innerHTML = 'Пожалуйста, выберите разные станции';
       return;
     }
 
-    let stationFromId : number = -1;
-    let stationToId : number = -1;
-    Object.keys(this.station_id_dict).forEach(key =>{
+    let stationFromId = -1;
+    let stationToId = -1;
+    Object.keys(this.station_id_dict).forEach(key => {
 
-      if (this.station_id_dict[key].station === this.stationFrom)
+      if (this.station_id_dict[key].station === this.stationFrom) {
         stationFromId = this.station_id_dict[key].id;
+      }
 
-      if (this.station_id_dict[key].station === this.stationTo)
+      if (this.station_id_dict[key].station === this.stationTo) {
         stationToId = this.station_id_dict[key].id;
-    })
+      }
+    });
 
-    let data = this._http.get("http://localhost:8080/course/api/path/min/"+stationFromId+"/"+stationToId,{ observe: 'response' })
-    .subscribe(data=>
-    {
-      res.innerHTML = "Время прибытия составляет " + Object.values(data.body)[1] + 
-      ";<div>Добраться можно по следующему алгоритму : " + Object.values(data.body)[0] + "</div>";
-      console.log(data);    
+    this._http.get('http://localhost:8080/course/api/path/min/' + stationFromId + '/' + stationToId,
+      { observe: 'response' })
+    .subscribe(data => {
+      const time = Object.values(data.body)[1];
+      const min = Math.floor(time / 60);
+      const sec = time - min * 60;
+      res.innerHTML = 'Время прибытия составляет ' + min + ':' + sec.toString().padStart(2, '0') + ' минут' +
+      ';<div>Добраться можно по следующему алгоритму : ' + Object.values(data.body)[0] + '</div>';
+      console.log(data);
     });
 
   }
@@ -89,16 +87,18 @@ export class MainpageComponent implements OnInit {
     this.fillSelectors();
   }
 
- selectorOneChanged(event){
+ selectorOneChanged(event) {
    this.stationFrom = event.target.value;
-   if ( this.stationTo != null )
+   if ( this.stationTo != null ) {
      this.fillPathResult();
+   }
  }
 
- selectorTwoChanged(event){
+ selectorTwoChanged(event) {
    this.stationTo = event.target.value;
-   if ( this.stationFrom != null )
+   if ( this.stationFrom != null ) {
      this.fillPathResult();
+   }
  }
 
 }
