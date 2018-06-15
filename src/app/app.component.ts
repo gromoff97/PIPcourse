c0592b38bbf1800c3b80f87eed6ae9bbf3deb904
SpaceStar <spacestar.ru@gmail.com>
2018-06-15 15:06:05 +0300

    add block for notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,12 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent implements OnInit {
   static apiUrl = 'http://localhost:8080/course/api';
+
+  private timer;
   _http: HttpClient;
   _stompService: StompService;
+
+  notification: string;
 
   constructor( private gotHttp: HttpClient, private gotStompService: StompService ) {
     this._http = gotHttp;
@@ -23,11 +27,18 @@ export class AppComponent implements OnInit {
     this._http.get(AppComponent.apiUrl + '/queue/new', { observe: 'response', responseType: 'text'})
       .subscribe(data => {
         const queueName: string = data.body;
-               const stomp_subscription = this._stompService
+               this._stompService
                  .subscribe(queueName, {'durable': false, 'auto-delete': true, 'exclusive': false})
                  .subscribe((msg: Message) => {
-                   console.log(`Received: ${msg.body}`);
+                   this.notification = msg.body;
+                   document.getElementById('notification').hidden = false;
+                   clearTimeout(this.timer);
+                   this.timer = setTimeout(this.notifClose, 5000);
                  });
       });
+  }
+
+  notifClose() {
+    document.getElementById('notification').hidden = true;
   }
 }
