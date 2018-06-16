4447ad0fc49246c042e75799e9940e0f228e1387
SpaceStar <spacestar.ru@gmail.com>
2018-06-16 09:00:07 +0300

    fill profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AppComponent, User, Validate} from '../app.component';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   ngOnInit() {
+    this.fillFields();
+  }
+
+  private fillFields() {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept-API-Version', 'resource=2.1');
+    this._http.post(AppComponent.amUrl + '/json/sessions?_action=validate', null,
+      {headers: headers, observe: 'response', withCredentials: true})
+      .subscribe(validate => {
+        const result = <Validate>validate.body;
+        if (result.valid) {
+          const user = result.uid;
+          this._http.get(AppComponent.amUrl + '/json/users/' + user,
+            {headers: headers, observe: 'response', withCredentials: true})
+            .subscribe(users => {
+              const userinfo = <User>users.body;
+              if (userinfo.mail) {
+                (<HTMLInputElement>document.getElementById('email')).value = userinfo.mail[0];
+                console.log('mail');
+              }
+              if (userinfo.sn) {
+                (<HTMLInputElement>document.getElementById('FIO')).value = userinfo.sn[0];
+                console.log('sn');
+              }
+              if (userinfo.givenName) {
+                (<HTMLInputElement>document.getElementById('FIO')).value += ' ' + userinfo.givenName[0];
+                console.log('name');
+              }
+            });
+        }
+      });
   }
 
 }
